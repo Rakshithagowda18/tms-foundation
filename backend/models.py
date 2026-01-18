d3aa9560d3f4f8ee3c96615e32d0c2c012c34636
Rakshithagowda18 <rakshagowda2244@gmail.com>
2026-01-18 19:17:03 +0530

    Add Donation model with fields for name and amount

---
+++ backend/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Donation(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
